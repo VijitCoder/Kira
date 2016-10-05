f16590e6038e713ad5e95f1f90c38ed5545044cf
Vijit <vijit@mail.ru>
2016-10-05 18:06:32 +0700

    Удалил мастер приложения и каталог engine

--- install/pieces/i18n/en.php
+++
@@ -1,9 +0,0 @@
-<?php
-// русский текст => перевод
-return [
-    'Язык' => 'Language',
-    'Добро пожаловать на сайт DOMAIN' => 'Welcome to site DOMAIN',
-    'Это пример только что созданного приложения. Теперь дело за вами..' => 'This is an example of the created application. Now, it\'s your turn..',
-    'Пример js-перевода' => 'An example of the js-translate',
-    'Нажми меня' => 'Click me',
-];
