06e73f77ffb179f64daa95dd4828a9b77fa3f714
Vijit <vijit@mail.ru>
2016-10-23 22:08:59 +0700

    utils\Strings. Unit-тесты

---
+++ tests/utils/StringsTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use kira\utils\Strings;
+
+/**
+ * Тестируем утилиту по работе со строками
+ */
+class StringsTest extends TestCase
+{
+    public function test_declination()
+    {
+        $words = ['комментарий', 'комментария', 'комментариев'];
+
+        $this->assertEquals('1 комментарий', Strings::declination(1, $words));
+        $this->assertEquals('2 комментария', Strings::declination(2, $words));
+        $this->assertEquals('5 комментариев', Strings::declination(5, $words));
+    }
+
+    public function test_translit()
+    {
+        $this->assertEquals(
+            'shodni korablya mokryie ot rosyi',
+            Strings::rus2eng('сходни корабля мокрые от росы'),
+            'Транслит русский > английский'
+        );
+
+        $this->assertEquals(
+            'ёжик по травке бежит и хохочет, ежику травка пузик щекочет',
+            Strings::eng2rus('jojik po travke bejit i hohochet, ejiku travka puzik schekochet'),
+            'Транслит английский > русский'
+        );
+    }
+
+    /**
+     * Приведение к регистру первых букв в мультибайтной кодировке
+     */
+    public function test_firstLetter()
+    {
+         $this->assertEquals('Русский', Strings::mb_ucfirst('русский'), 'Заглавная первая буква');
+         $this->assertEquals('пРОБЕЛ', Strings::mb_lcfirst('ПРОБЕЛ'), 'Строчная первая буква');
+    }
+
+    public function test_word_chunk()
+    {
+        $this->assertEquals('рус + ски + й', Strings::word_chunk('русский', 3, ' + '), 'Мультибайтное разбиение строки');
+    }
+}
