d260348dbc9a922c84d8a0e28106d613b290da9f
Vijit <vijit@mail.ru>
2017-09-21 12:55:09 +0700

    Валидация. Заменил Bounds и Length на общий валидатор Limits.

---
+++ src/validation/validators/Limits.php
@@ -0,0 +1,138 @@
+<?php
+namespace kira\validation\validators;
+
+use kira\core\App;
+use kira\exceptions\FormException;
+
+/**
+ * Проверка значения на соблюдение заданных пределов. Для числел - диапазон значений, для строк - длина строки.
+ *
+ * Настройки валидатора:
+ * <pre>
+ * $options = [
+ *     'min'     => number,
+ *     'max'     => number,
+ *     'message' => string|array [min => string, max => string],
+ * ];
+ * </pre>
+ *
+ * Любой из параметров можно пропустить. Значение 'NULL' - не проверять границу с этой стороны.
+ */
+class Limits extends AbstractValidator
+{
+    /**
+     * Тип валидируемого значения. Приватные константы класса.
+     * @internal
+     */
+    const
+        TYPE_NUMBER = 'number',
+        TYPE_STRING = 'string';
+
+    /**
+     * Дефолтные сообщения об ошибках. Библиотека
+     * @var array
+     */
+    private $messageLibrary = [
+        self::TYPE_NUMBER => [
+            'min' => 'Значение меньше допустимого, минимум :min',
+            'max' => 'Значение больше допустимого, максимум :max',
+        ],
+        self::TYPE_STRING => [
+            'min' => 'Слишком короткое значение, минимум :min символов',
+            'max' => 'Слишком длинное значение, максимум :max символов',
+        ],
+    ];
+
+    /**
+     * Нормализация настроек валидатора. Полностью переопределяем родительский конструктор.
+     *
+     * Если настройки заданы не массивом, очевидно нечего проверять. Ставим заглушку и выходим из метода.
+     *
+     * Приведение к FLOAT шире, чем нужно для длины строки, там достаточно INT. Но это не нарушит логику проверки,
+     * зато позволяет обобщить код.
+     *
+     * @param mixed $options
+     */
+    public function __construct($options = [])
+    {
+        if (!is_array($options)) {
+            $this->options = null;
+            return;
+        }
+
+        $options['min'] = isset($options['min']) ? (float)$options['min'] : null;
+        $options['max'] = isset($options['max']) ? (float)$options['max'] : null;
+
+        $this->options = $options;
+
+        $this->prepareMessages();
+    }
+
+    /**
+     * Подготовка библиотеки сообщений об ошибках
+     *
+     * Если клиент задал свое сообщение(я), тогда заменяем дефолтные на его вариант. При этом ожидаем либо массив двух
+     * сообщений [min, max] либо одно и тоже сообщение ставим на обе ошибки.
+     *
+     * Замена подстановок min/max на значения, заданные в настройках валидатора.
+     */
+    private function prepareMessages()
+    {
+        $minSubstitute = $this->options['min'] ?? 'NULL';
+        $maxSubstitute = $this->options['max'] ?? 'NULL';
+
+        $customMessage = $this->options['message'] ?? null;
+
+        foreach ($this->messageLibrary as &$set) {
+            if (!is_null($customMessage)) {
+                $set = is_array($customMessage)
+                    ? array_merge($set, $customMessage)
+                    : [
+                        'min' => $customMessage,
+                        'max' => $customMessage,
+                    ];
+            }
+            $set = [
+                'min' => App::t($set['min'], [':min' => $minSubstitute]),
+                'max' => App::t($set['max'], [':max' => $maxSubstitute]),
+            ];
+        }
+    }
+
+    /**
+     * Валидатор проверки длины строки или диапазона числа
+     * @param mixed $value проверяемое значение
+     * @return bool
+     * @throws FormException
+     */
+    public function validate($value)
+    {
+        if (is_null($this->options)) {
+            return true;
+        }
+
+        if (is_string($value)) {
+            $value = mb_strlen(strval($value));
+            $messageSet = $this->messageLibrary[self::TYPE_STRING];
+        } else if (is_numeric($value)) {
+            $value *= 1; // приведение к типу
+            $messageSet = $this->messageLibrary[self::TYPE_NUMBER];
+        } else {
+            throw new FormException('Проверка границ не применима к текущему типу данных: ' . gettype($value));
+        }
+
+        $min = $this->options['min'];
+        $max = $this->options['max'];
+
+        $passed = true;
+        if (!is_null($min) && $value < $min) {
+            $passed = false;
+            $this->options['message'] = $messageSet['min'];
+        } else if (!is_null($max) && $value > $max) {
+            $passed = false;
+            $this->options['message'] = $messageSet['max'];
+        }
+
+        return $passed;
+    }
+}
---
+++ tests/validation/validators/LimitsTest.php
@@ -0,0 +1,120 @@
+<?php
+use kira\exceptions\FormException;
+use kira\validation\validators\Limits;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Тестируем валидатор Limits
+ */
+class LimitsTest extends TestCase
+{
+    /**
+     * Проверка диапазона числа. Дефолтные сообщения, оба предела
+     */
+    public function test_limits_number()
+    {
+        $validator = new Limits([
+            'min' => '4.56',
+            'max' => 10.2,
+        ]);
+
+        $this->assertTrue($validator->validate(4.56));
+        $this->assertTrue($validator->validate(7));
+        $this->assertTrue($validator->validate(10.2));
+
+        $this->assertFalse($validator->validate(3));
+        $this->assertFalse($validator->validate(12));
+
+        $this->expectException(FormException::class);
+        $validator->validate(['one']);
+    }
+
+    /**
+     * Проверка длины строки. Дефолтные сообщения, оба предела
+     */
+    public function test_limits_string()
+    {
+        $validator = new Limits([
+            'min' => 5,
+            'max' => 10,
+        ]);
+
+        $this->assertTrue($validator->validate('plate'));      // 5 символов
+        $this->assertTrue($validator->validate('тарелка'));    // 7 символов
+        $this->assertTrue($validator->validate('supervisor')); // 10 символов
+
+        $this->assertFalse($validator->validate('нет'));
+        $this->assertFalse($validator->validate('tyrannosaur Rex')); // 15 символов
+    }
+
+    /**
+     * Один предел или вообще без ограничений
+     */
+    public function test_no_limit()
+    {
+        $validator = new Limits([
+            'min' => 5,
+        ]);
+
+        $this->assertTrue($validator->validate(10));
+        $this->assertTrue($validator->validate(100043));
+        $this->assertFalse($validator->validate(3));
+
+        $validator = new Limits([
+            'max' => 10,
+        ]);
+
+        $this->assertTrue($validator->validate(-10005));
+        $this->assertTrue($validator->validate(10));
+        $this->assertFalse($validator->validate(12));
+
+        // Валидация границ с такими настройками не имеет смысла, но не должна падать.
+        $this->assertTrue((new Limits(false))->validate(234));
+        $this->assertTrue((new Limits)->validate(100500));
+    }
+
+    /**
+     * Кастомные соообщения об ошибках
+     */
+    public function test_custom_message()
+    {
+        $validator = new Limits([
+            'min'     => 5,
+            'max'     => 10,
+            'message' => 'Общее сообщение на ошибки',
+        ]);
+
+        $this->assertFalse($validator->validate(3));
+        $this->assertEquals('Общее сообщение на ошибки', $validator->error);
+        $this->assertFalse($validator->validate(12));
+        $this->assertEquals('Общее сообщение на ошибки', $validator->error);
+
+        $validator = new Limits([
+            'min'     => 5,
+            'max'     => 10,
+            'message' => [
+                'min' => 'Число не меньше :min',
+                'max' => 'Слишком большое число',
+            ],
+        ]);
+
+        $this->assertFalse($validator->validate(3));
+        $this->assertEquals('Число не меньше 5', $validator->error);
+        $this->assertFalse($validator->validate(12));
+        $this->assertEquals('Слишком большое число', $validator->error);
+    }
+
+    /**
+     * Тест на угадывание правильного типа проверяемого значения
+     */
+    public function test_guess()
+    {
+        $validator = new Limits([
+            'min'     => 2,
+            'max'     => 10,
+        ]);
+
+        $this->assertTrue($validator->validate('77'), 'Значение, как строка, верное');
+        $this->assertFalse($validator->validate(77), 'Значение, как число, не входит в разрешенный диапазон');
+    }
+}
