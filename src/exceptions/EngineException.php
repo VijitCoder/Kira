bb5f66311d657cdc2be7ae38c5d9f867e4f9ea92
Vijit <vijit@mail.ru>
2018-10-04 15:58:18 +0700

    Fix: typehint в EngineException был не в том пространстве имен.

--- src/exceptions/EngineException.php
+++ src/exceptions/EngineException.php
@@ -57,9 +57,9 @@ class EngineException extends \Exception
      *
      * @param string         $message
      * @param int            $code
-     * @param Throwable|null $previous
+     * @param \Throwable|null $previous
      */
-    public function __construct($message = '', $code = 0, Throwable $previous = null)
+    public function __construct($message = '', $code = 0, \Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
         $trace = $this->getTrace();
